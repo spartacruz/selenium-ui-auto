2f6a06af23a98596d24c43598ac72e635cde6538
Yuri <yuri.barru@gmail.com>
2023-08-04 22:16:10 +0700

    add several new test case, login related

--- src/test/login-test.js
+++ src/test/login-test.js
@@ -58,6 +58,41 @@ describe('Login Page Test', function() {
         sleep(5)
     });
 
+    it('LO_7 - (-) Login with blank email and valid password', async function() {
+        await loginPage.goToPage();
+        await loginPage.inputPasswordField("Monotaroid1!");
+        await loginPage.clickSubmitBtn();
+        warningEmailField = await loginPage.warningFailedFieldRequired("email");
+        warningEmailField = await warningEmailField.getText();
+        expect(warningEmailField).equal(loginPage.warningRequiredField);
+        sleep(5)
+    });
+
+    it('LO_8 - (-) Login with valid email and blank password', async function() {
+        await loginPage.goToPage();
+        await loginPage.inputUsernameField("monotaroid@yopmail.com");
+        await loginPage.clickSubmitBtn();
+        warningPasswordField = await loginPage.warningFailedFieldRequired("password");
+        warningPasswordField = await warningPasswordField.getText();
+        expect(warningPasswordField).equal(loginPage.warningRequiredField);
+        sleep(5)
+    });
+
+    it.only('LO_9 - (-) Login with blank email and blank password', async function() {
+        await loginPage.goToPage();
+        await loginPage.clickSubmitBtn();
+        warningPasswordField = loginPage.warningFailedFieldRequired("password");
+
+        warningEmailField = await loginPage.warningFailedFieldRequired("email");
+        warningEmailField = await warningEmailField.getText();
+        expect(warningEmailField).equal(loginPage.warningRequiredField);
+
+        warningPasswordField = await loginPage.warningFailedFieldRequired("password");
+        warningPasswordField = await warningPasswordField.getText();
+        expect(warningPasswordField).equal(loginPage.warningRequiredField);
+        sleep(5)
+    });
+
     function sleep(second) {
         return new Promise(resolve => setTimeout(resolve, second*1000));
     }
