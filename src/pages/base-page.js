e50a17780da546253e1a134f0dd4961a01d2d1a4
Yuri <yuri.barru@gmail.com>
2023-08-06 10:24:37 +0700

    Seperate driverbuilder and add function refreshSession on basePage

--- src/pages/base-page.js
+++ src/pages/base-page.js
@@ -11,25 +11,40 @@
 
 const chrome = require('selenium-webdriver/chrome')
 const webdriver = require('selenium-webdriver')
+const logging = require('selenium-webdriver/lib/logging')
 
-let options = new chrome.Options()
-options.addArguments(["--disable-blink-features=AutomationControlled", "start-maximized", "disable-infobars", "--disable-extensions"])
-options.excludeSwitches('enable-logging')
-let driver = new webdriver.Builder()
- .withCapabilities(webdriver.Capabilities.chrome())
- .setChromeOptions(options)
- .build()
+function buildDriver(){
+    let options = new chrome.Options()
+    options.addArguments(["--disable-blink-features=AutomationControlled", "start-maximized", "disable-infobars", "--disable-extensions"])
+    options.excludeSwitches('enable-logging')
+    let driver = new webdriver.Builder()
+    .withCapabilities(webdriver.Capabilities.chrome())
+    .setChromeOptions(options)
+    .build()
+
+    return driver;
+}
 
 
 class BasePage {
     constructor(){
-        this.driver = driver;
+        this.driver = buildDriver();
+
+        //set logging to severe only
+        this.logger = logging.getLogger('webdriver');
+        this.logger.setLevel(logging.Level.INFO)
     }
     async goToUrl(url){
-        await driver.get(url);
+        await this.driver.get(url);
     }
     async closeBrowser(){
-        await driver.close()
+        await this.driver.close();
+        await this.driver.quit();
+    }
+
+    async refreshSession(){
+        // await this.driver.reloadSession();
+        this.driver = await buildDriver();
     }
 };
 
