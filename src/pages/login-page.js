023ce9c00cba23c973c98ee80745b05b0ebf6ff3
Yuri <yuri.barru@gmail.com>
2023-08-06 10:23:30 +0700

    add wait-ing condition elementLocated before isVisible to reduce element not found Group validation located n visible in a function

--- src/pages/login-page.js
+++ src/pages/login-page.js
@@ -1,7 +1,7 @@
 const { By } = require('selenium-webdriver');
 const { until } = require('selenium-webdriver');
 let BasePage = require('../pages/base-page.js');
-const WAITING_ELEMENT_TIMEOUT = 15000 //milis
+const WAITING_ELEMENT_TIMEOUT = 10000 //milis
 
 class LoginPage extends BasePage {
     
@@ -10,7 +10,7 @@ class LoginPage extends BasePage {
         this.usernameField = By.id("email");
         this.passwordField = By.id("password");
         this.loginBtn = By.xpath("//button[@type='submit']");
-        this.loginFailedWarning = By.xpath("//div[contains(@class, 'message-error')]");
+        this.loginFailedWarning = By.className("message-error error message");
         this.emailRequiredWarning = By.id("email-error");
         this.passwordRequiredWarning = By.id("password-error");
 
@@ -23,30 +23,27 @@ class LoginPage extends BasePage {
     }
 
     async webElementFactory(locator){
+        await this.validateElementLocatedAndVisible(this.locator);
         return this.driver.findElement(locator);
     }
 
     async inputUsernameField(userName) {
-        // this.webElementUsernameField = await this.driver.findElement(this.usernameField);
-        await this.driver.wait(until.elementIsVisible(this.driver.findElement(this.usernameField)), WAITING_ELEMENT_TIMEOUT);
+        await this.validateElementLocatedAndVisible(this.usernameField);
         await this.driver.findElement(this.usernameField).sendKeys(userName);
     }
 
     async inputPasswordField(password) {
-        // this.webElementPasswordField = await this.driver.findElement(this.passwordField);
-        await this.driver.wait(until.elementIsVisible(this.driver.findElement(this.passwordField)), WAITING_ELEMENT_TIMEOUT);
+        await this.validateElementLocatedAndVisible(this.passwordField);
         await this.driver.findElement(this.passwordField).sendKeys(password);
     }
 
     async clickSubmitBtn(){
-        // this.webElementSubmitBtn = await this.driver.findElement(this.loginBtn);
-        await this.driver.wait(until.elementIsVisible(this.driver.findElement(this.loginBtn)), WAITING_ELEMENT_TIMEOUT);
+        await this.validateElementLocatedAndVisible(this.loginBtn);
         await this.driver.findElement(this.loginBtn).click();
     }
 
-    async warningFailedLoginHeader(){
-        // this.webElementLoginHeader = await this.driver.findElement(this.loginFailedWarning);
-        await this.driver.wait(until.elementIsVisible(this.driver.findElement(this.loginFailedWarning)), WAITING_ELEMENT_TIMEOUT);
+    async warningFailedLoginHeaderElement(){
+        await this.validateElementLocatedAndVisible(this.loginFailedWarning);
         return this.driver.findElement(this.loginFailedWarning);
     }
 
@@ -64,10 +61,17 @@ class LoginPage extends BasePage {
         if (field !== "email" && field !== "password") {
             throw "Must be email or password"
         }
-
-        // this.webElementFieldRequiredWarning = await this.driver.findElement(locator);
-        await this.driver.wait(until.elementIsVisible(this.driver.findElement(locator)), WAITING_ELEMENT_TIMEOUT);
+        await this.validateElementLocatedAndVisible(locator)
         return this.driver.findElement(locator);
+    }
+
+    async validateElementLocatedAndVisible(locator){
+        try {
+            await this.driver.wait(until.elementLocated(locator), WAITING_ELEMENT_TIMEOUT);
+            await this.driver.wait(until.elementIsVisible(this.driver.findElement(locator)), WAITING_ELEMENT_TIMEOUT);
+        } catch(err){
+            console.log(err)
+        }
     }
 };
 
